035e02100fd57b6f0561a1be6caaa0ca7da98705
Lyof <florian.lyof@laposte.net>
2022-08-25 16:31:05 +0200

    Second zone added

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 from functions.libs import *
 
-#generate('route/route1')
-addxp(1000)
-print(getdata()['fight']['stats']['pv'], maxstat('pv'))
-catch('pokeball', 1)
-
-#playsound('assets/music/fight_wild.wav')
+generate('fearful_forest_entrance')
